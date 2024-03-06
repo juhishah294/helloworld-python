6abf5689dc4add5efb4a7b864ece89fbc16d27fc
Juhi Dungrani <juhi@toystack.co>
2024-03-06 21:34:07 +0530

    chamged file structure

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask 
 app = Flask(__name__) 
   
-# routing the decorator function hello_name 
 @app.route('/')   
 def hello_name(): 
-   return 'Hello Toystack123!' 
+   return 'Hello Toystack App!' 
   
-
+if __name__ == '__main__': 
+   app.run(debug=True) 
